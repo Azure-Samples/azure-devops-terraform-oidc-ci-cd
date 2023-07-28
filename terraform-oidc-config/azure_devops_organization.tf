71220c760433594813771295b2b6de47949de713
Jared Holgate <jaredholgate@microsoft.com>
2023-07-28 14:42:23 +0100

    Update to use alternative API

--- terraform-oidc-config/azure_devops_organization.tf
+++ terraform-oidc-config/azure_devops_organization.tf
@@ -1,10 +1,10 @@
 locals {
-  azure_devops_organization_id = jsondecode(data.http.azure_devops_organization.response_body).value[0].id
+  azure_devops_organization_id = jsondecode(data.http.azure_devops_organization.response_body).instanceId
   azure_devops_base_64_pat = base64encode(":${var.azure_devops_token}")
 }
 
 data "http" "azure_devops_organization" {
-  url = "https://dev.azure.com/${var.azure_devops_organisation_target}/_apis/projectCollections"
+  url = "https://dev.azure.com/${var.azure_devops_organisation_target}/_apis/connectionData"
 
   request_headers = {
     Accept        = "application/json"
@@ -15,7 +15,7 @@ data "http" "azure_devops_organization" {
     postcondition {
       condition     = tonumber(self.status_code) < 300
       error_message = "Could not retrieve member information"
-    }
+    } 
   }
 }
 
