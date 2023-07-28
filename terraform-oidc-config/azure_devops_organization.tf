1f03509657511e7e233f0a77160a8e83d1305ed1
Jared Holgate <jaredholgate@microsoft.com>
2023-07-28 13:44:18 +0100

    Use temporary alternative

--- terraform-oidc-config/azure_devops_organization.tf
+++ terraform-oidc-config/azure_devops_organization.tf
@@ -1,13 +1,14 @@
 locals {
-  azure_devops_organization_id = [for a in jsondecode(data.http.azdo_organizations.response_body).value : a.accountId if a.accountName == var.azure_devops_organisation_target][0]
+  azure_devops_organization_id = jsondecode(data.http.azure_devops_organization.response_body).value[0].id
+  azure_devops_base_64_pat = base64encode(":${var.azure_devops_token}")
 }
 
-data "http" "azdo_member" {
-  url = "https://app.vssps.visualstudio.com/_apis/profile/profiles/me?api-version=7.1-preview.1"
+data "http" "azure_devops_organization" {
+  url = "https://dev.azure.com/${var.azure_devops_organisation_target}/_apis/projectCollections"
 
   request_headers = {
     Accept        = "application/json"
-    Authorization = "Bearer ${var.azure_devops_token}"
+    Authorization = "Basic ${local.azure_devops_base_64_pat}"
   }
 
   lifecycle {
@@ -18,17 +19,6 @@ data "http" "azdo_member" {
   }
 }
 
-data "http" "azdo_organizations" {
-  url = "https://app.vssps.visualstudio.com/_apis/accounts?api-version=7.1-preview.1&memberId=${jsondecode(data.http.azdo_member.response_body).id}"
-  request_headers = {
-    Accept        = "application/json"
-    Authorization = "Bearer ${var.azure_devops_token}"
-  }
-
-  lifecycle {
-    postcondition {
-      condition     = tonumber(self.status_code) < 300
-      error_message = "Could not retrieve account information"
-    }
-  }
+output "azure_devops_organization_id" {
+  value = local.azure_devops_organization_id
 }
